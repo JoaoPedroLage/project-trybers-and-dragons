4e093b2d0d8a0a4bc11d104b08309d330169ba89
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-04-20 16:50:30 -0300

    feat:  create the abstract class Race and finished req 1

---
+++ src/Races/Race.ts
@@ -0,0 +1,24 @@
+export default abstract class Race {
+  private static instanceCount = 0;
+  
+  constructor(private _name: string, private _dexterity: number) {
+    this._name = _name;
+    this._dexterity = _dexterity;
+  }
+  
+  get name() {
+    return this._name; 
+  }
+
+  get dexterity() {
+    return this._dexterity; 
+  }
+
+  abstract get maxLifePoints(): number;
+
+  static createdRacesInstances(): number {
+    if (this.instanceCount === 0) throw new Error('Not implemented');
+
+    return this.instanceCount;
+  }
+}
