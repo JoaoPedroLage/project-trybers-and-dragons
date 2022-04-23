e7a3076b3c99c4897a57c427d7c545ed9d5f74d8
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-04-22 23:45:54 -0300

    feat: just start req 7

---
+++ src/Character.ts
@@ -0,0 +1,19 @@
+// import Fighter from './Fighter';
+// import Race from './Races';
+// import Archetype from './Archetypes';
+// import Energy from './Energy';
+
+// class Character implements Fighter {
+//   private _race: Race;
+//   private _archetype: Archetype;
+//   private _maxLifePoints: number;
+//   private _lifePoints: number;
+//   private _strength: number;
+//   private _defense: number;
+//   private _dexterity: number;
+//   private _energy: Energy;
+
+//   constructor(){
+
+//   }
+// }
