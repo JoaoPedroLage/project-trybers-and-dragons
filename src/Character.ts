a86667276e4c7adbcab5d7e7468aef61793880aa
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-04-25 00:29:00 -0300

    feat: finish the class Character

--- src/Character.ts
+++ src/Character.ts
@@ -1,19 +1,91 @@
-// import Fighter from './Fighter';
-// import Race from './Races';
-// import Archetype from './Archetypes';
-// import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import getRandomInt from './utils';
 
-// class Character implements Fighter {
-//   private _race: Race;
-//   private _archetype: Archetype;
-//   private _maxLifePoints: number;
-//   private _lifePoints: number;
-//   private _strength: number;
-//   private _defense: number;
-//   private _dexterity: number;
-//   private _energy: Energy;
+export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
 
-//   constructor(){
+  constructor(name: string) {
+    this._race = new Elf(name, 10);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = Math.floor(Math.random() * 10 + 1);
+    this._defense = Math.floor(Math.random() * 10 + 1);
+    // site com o conteudo usado para realizar a função acima:
+    // http://devfuria.com.br/javascript/numeros-aleatorios/#:~:text=n%C3%BAmero%20pseudo%2Daleat%C3%B3rio.-,Math.,dimensionar%20para%20o%20intervalo%20desejado.
+    this._dexterity = this._race.dexterity;
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: Math.floor(Math.random() * 10 + 1),
+    };
+    // foi visto depois que havia a função getRandomInt(1, 10) no arquivo utils para fazer esse processo
+  }
 
-//   }
-// }
+  get race(): Race {
+    return this._race;
+  }
+
+  get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+
+  get dexterity(): number {
+    return this._defense;
+  }
+
+  get energy(): Energy {
+    return { ...this._energy };
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this._defense;
+
+    if (damage > 0) { this._lifePoints -= damage; }
+
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+
+    return this._lifePoints;
+  }
+
+  attack(enemy : Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    this._maxLifePoints += getRandomInt(1, 10);
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._energy.amount = 10;
+    
+    if (this._maxLifePoints >= this.race.maxLifePoints) {
+      this._maxLifePoints = this.race.maxLifePoints;
+    }
+
+    this._lifePoints = this._maxLifePoints;
+  }
+
+  special() : void { this._strength = 1000; }
+}
